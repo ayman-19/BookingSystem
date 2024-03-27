ccb4012154d860f296401405d5e0f7e83c00b4b3
ayman <aymanroshdy909@gmail.com>
2024-03-27 23:11:12 +0200

    first commit

---
+++ BookingSystem.Presistance/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingSystem.Application.IRepositories;
+using BookingSystem.Domain.Model;
+using BookingSystem.Presistance.Data;
+
+namespace BookingSystem.Presistance.Repositories
+{
+	public class RefreshTokenRepository : Repository<RefreshToken>, IRefreshTokenRepository
+	{
+		public RefreshTokenRepository(BookingDbContext context) : base(context)
+		{
+		}
+	}
+}
